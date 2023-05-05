c487a764f2b4a4fd58e8d3e33cbb30b7d7a7a913
chrisJohns0n <126682002+ChrisJohnson0110@users.noreply.github.com>
2023-05-05 19:16:45 +0100

    CameraSwappingScript

--- Assets/Scripts/CameraSwapper.cs
+++ Assets/Scripts/CameraSwapper.cs
@@ -8,30 +8,76 @@ using UnityEngine;
 
 public class CameraSwapper : MonoBehaviour
 {
-    [SerializeField] Camera[] cSwappableCameras;
-
-
+    List<Camera> li_cSwappableCameras; //list of all camera that can be swapped to
+    private int iCurrentCamera; //current camera to display
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        li_cSwappableCameras = new List<Camera>(FindObjectsOfType<Camera>()); //get all active cameras
+        if (li_cSwappableCameras == null) //set current to inactive hiding camera controls
+        {
+            gameObject.SetActive(false);
+        }
+        SetActiveCamera(); //set the camera in list pos 0 
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// set all cameras visability to arg 
+    /// </summary>
+    private void DeactivateAllCameras()
     {
-        
+        //update all cameras to arg
+        foreach (Camera c in li_cSwappableCameras)
+        {
+            c.gameObject.SetActive(false);
+        }
     }
 
+    /// <summary>
+    /// change the current display camera to a new one
+    /// </summary>
+    /// <param Camera num to set="a_iCamera"></param>
+    private void SetActiveCamera()
+    {
+        DeactivateAllCameras(); //hide all cameras
+        li_cSwappableCameras[iCurrentCamera].gameObject.SetActive(true); //set desired to active
+    }
+
+    //Button functions
+
+    /// <summary>
+    /// change the current camera int to one lower in list - rolls over
+    /// </summary>
     public void GoBackACamera()
     {
-
+        //decrease current camera value
+        if (iCurrentCamera == 0)
+        {
+            iCurrentCamera = li_cSwappableCameras.Count - 1;
+        }
+        else
+        {
+            iCurrentCamera--;
+        }
+        SetActiveCamera(); //set active camera
     }
 
+    /// <summary>
+    /// change the current camera int to the next in list - rolls over
+    /// </summary>
     public void GoForwardACamera()
     {
-
+        //increase current camera value
+        if (iCurrentCamera == li_cSwappableCameras.Count - 1)
+        {
+            iCurrentCamera = 0;
+        }
+        else
+        {
+            iCurrentCamera++;
+        }
+        SetActiveCamera(); //set active camera
     }
 
 
